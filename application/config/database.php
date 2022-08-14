a5af6cf9484a1bca4347b5babad328fa44ef23f6
abdulaziz <abdu19010ti@student.nurulfikri.ac.id>
2022-08-14 15:46:03 +0700

    payment duitku insert and update db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'kampuskode.com',
-	'username' => 'kamg4647_admin',
-	'password' => 'kampuskode99',
-	'database' => 'kamg4647_paymentku',
+	'hostname' => 'localhost',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
